6096bcce058eb2cfa534a56056c53d6bac247fa7
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-08-31 20:05:21 +0530

    u

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import "./styles.css";
+import React from "react";
+import { ChatEngine } from "react-chat-engine";
+import Feed from "./components/ChatFeed";
+import LoginForm from "./components/Login";
 
-export default function App() {
+const projectID = "ce90368b-2e78-4ce8-8627-4beb1e7fa462";
+const App = () => {
+  if (!localStorage.getItem("usename")) {
+    return <LoginForm />;
+  }
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <Feed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+        ).play()
+      }
+    />
   );
-}
+};
+export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import { React, useState } from "react";
 import axios from "axios";
 
-const projectId = "";
+const projectId = "ce90368b-2e78-4ce8-8627-4beb1e7fa462";
 
 const LoginForm = () => {
   const [username, setUsername] = useState("");
