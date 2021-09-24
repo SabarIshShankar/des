06d343553e501b735d891586f3028d3a4ba7732b
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-09-24 11:04:19 +0530

    u

---
+++ components/Boiler.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  AppRegistry,
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  Modal,
+  TouchableHighlight,
+  TouchableOpacity,
+  Platform,
+  DeviceeventEmitter
+} from "react-native";
+
+import { createIconSet } from "react-native-vector-icons";
+import Button from "react-native-button";
+import VoxImplant from "react-native-voximplant";
+
+var update = require("react-addons-update");
