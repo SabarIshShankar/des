95808a83b29d6d856f095b159394055b6e93f0a9
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-09-24 11:12:56 +0530

    u boiler

--- components/Boiler.js
+++ components/Boiler.js
@@ -9,7 +9,7 @@ import {
   TouchableHighlight,
   TouchableOpacity,
   Platform,
-  DeviceeventEmitter
+  DeviceEventEmitter
 } from "react-native";
 
 import { createIconSet } from "react-native-vector-icons";
@@ -17,3 +17,46 @@ import Button from "react-native-button";
 import VoxImplant from "react-native-voximplant";
 
 var update = require("react-addons-update");
+
+import ColorSwitch from "./ColorSwitch";
+import ToggleButton from "./ToggleButton";
+import { Keypad } from "./Keypad";
+
+var glyphMap = {
+  speaker: "\uE600",
+  "mic-mute": "\uE601",
+  keypad: "\uE602",
+  "snd-mute": "\uE603",
+  phone: "\uE604",
+  hangup: "\uE605",
+  "flip-camera": "\uE606"
+};
+
+if (Platform.OS == "ios") {
+  var Icon = createIconSet(glyphMap, "icomoon");
+} else {
+  Icon = createIconSet(glyphMap, "Custom");
+}
+var currentCallId,
+  uaInstance,
+  number = "",
+  settings_p2p = false,
+  settings_video = false,
+  micMutes = false,
+  loudSpeaker = false,
+  sendVideo = true,
+  camera = "front";
+
+DeviceEventEmitter.addListener("Callinging", (callId) => {
+  console.log("Call ringing");
+});
+
+DeviceEventEmitter.addListener("CallConnected", (callId) => {
+  console.log("Call connected");
+  uaInstance.callConncted(callId);
+});
+
+DeviceEventEmitter.addListener("CallFailed", (callId, code, reason) => {
+  console.log("Call failed. Code" + code + "Reason" + reason);
+  uaInstance.setModalText("Call failed", "idle");
+});
