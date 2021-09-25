4fcc4d1b7865bb3287ea953494f63217c4091f88
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-09-25 16:08:43 +0530

    u

--- components/Boiler.js
+++ components/Boiler.js
@@ -1,26 +1,15 @@
-import React, { Component } from "react";
-import {
-  AppRegistry,
-  Text,
-  View,
-  StyleSheet,
-  TextInput,
-  Modal,
-  TouchableHighlight,
-  TouchableOpacity,
-  Platform,
-  DeviceEventEmitter
-} from "react-native";
+import React, {Component} from 'react';
+import {AppRegistry, Text, View, StyleSheet, TextInput, Modal, TouchableHighlight, TouchableOpacity, Platform, DeviceEventEmitter} from 'react-native';
 
-import { createIconSet } from "react-native-vector-icons";
-import Button from "react-native-button";
-import VoxImplant from "react-native-voximplant";
+import {createIconSet} from 'react-native-vector-icons';
+import Button from 'react-native-button';
+import VoxImplant from 'react-native-voximplant';
 
-var update = require("react-addons-update");
+var update = require('react-addons-update');
 
-import ColorSwitch from "./ColorSwitch";
-import ToggleButton from "./ToggleButton";
-import { Keypad } from "./Keypad";
+import ColorSwitch from './ColorSwitch';
+import ToggleButton from './ToggleButton';
+import {Keypad} from './Keypad';
 
 var glyphMap = {
   speaker: "\uE600",
@@ -30,28 +19,28 @@ var glyphMap = {
   phone: "\uE604",
   hangup: "\uE605",
   "flip-camera": "\uE606"
-};
+}
 
-if (Platform.OS == "ios") {
-  var Icon = createIconSet(glyphMap, "icomoon");
+if(Platform.OS == 'ios'){
+  var Icon = createIconSet(glyphMap, "icomoon")
 } else {
-  Icon = createIconSet(glyphMap, "Custom");
+  Icon = createIconSet(glyphMap, "Custom")
 }
 var currentCallId,
   uaInstance,
   number = "",
   settings_p2p = false,
   settings_video = false,
-  micMutes = false,
+  micMuted= false,
   loudSpeaker = false,
   sendVideo = true,
   camera = "front";
 
-DeviceEventEmitter.addListener("Callinging", (callId) => {
-  console.log("Call ringing");
+DeviceEventEmitter.addListener("Callinging", callId => {
+  console.log("Call ringing")
 });
 
-DeviceEventEmitter.addListener("CallConnected", (callId) => {
+DeviceEventEmitter.addListener("CallConnected", callId => {
   console.log("Call connected");
   uaInstance.callConncted(callId);
 });
@@ -59,14 +48,14 @@ DeviceEventEmitter.addListener("CallConnected", (callId) => {
 DeviceEventEmitter.addListener("CallFailed", (callId, code, reason) => {
   console.log("Call failed. Code" + code + "Reason" + reason);
   uaInstance.setModalText("Call failed", "idle");
-});
+})
 
-DeviceEventEmitter.addListener("CallDisconnected", (callId) => {
+DeviceEventEmitter.addListener("CallDisconnected", callId => {
   console.log("Call disconnected");
   uaInstance.callDisconnected(callId);
 });
 
-DeviceEventEmitter.addListener("IncomingCall", (incomingCall) => {
+DeviceEventEmitter.addListener("IncomingCall", incomingCall => {
   console.log("Inbound Call");
   currentCallId = incomingCall.callId;
   uaInstance.setModalText(
@@ -75,8 +64,9 @@ DeviceEventEmitter.addListener("IncomingCall", (incomingCall) => {
   );
 });
 
-export default class Boiler extends Component {
-  constructor() {
+
+export default class Boiler extends Component{
+  constructor(){
     super();
     this.state = {
       modalText: "",
@@ -86,27 +76,25 @@ export default class Boiler extends Component {
     VoxImplant.SDK.switchToCamera(camera);
   }
 
-  componentDidMount() {
+  componentDidMount(){
     uaInstance = this;
     this._thisNumber.focus();
   }
 
-  updateNumber(text) {
+  updateNumber(text){
     number = text;
-    this._thisNumber.setNativeProps({ text: text });
+    this._thisNumber.setNativeProps({text: text});
   }
-  onSubmit(event) {
+  onSubmit(event){
     this.makeCall();
   }
-  makeCall(event) {
+  makeCall(event){
     console.log("calling" + number);
     VoxImplant.SDK.createCall(
-      number,
-      settings_video,
-      null,
-      function (callId) {
-        if (settings_p2p)
-          VoxImplant.SDK.startCall(callId, { "X-DirectCall": "true" });
+      number, settings_video, null,
+      function(callId){
+        if(settings_p2p)
+          VoxImplant.SDK.startCall(callId, {"X-DirectCall": "true"});
         else VoxImplant.SDK.startCall(callId);
         this.setState(
           update(this.state, {
@@ -118,4 +106,104 @@ export default class Boiler extends Component {
       }.bind(this)
     );
   }
+
+  cancelCall(event){
+    console.log("Cancel call");
+    VoxImplant.SDK.disconnectCall(currentCallId);
+  }
+
+  answerCall(){
+    VoxImplant.SDK.answerCall(currentCallId);
+    this.closeModal(true)
+  }
+
+  rejectCall(){
+    VoxImplant.SDK.declineCall(currentCallId);
+    this.closeModal(true);
+  }
+  callConnected(callId){
+    this.setState(
+      update(this.state, {
+        $merge: {
+          status: "connected"
+        }
+      })
+    );
+  }
+
+  callDisconnected(callId){
+    number = "";
+    loudSpeaker = false;
+    micMuted = false;
+    sendVideo = true;
+    VoxImplant.SDK.setUseLoudSpeaker(loudSpeaker);
+    VoxImplant.SDK.setMute(micMuted);
+    VoxImplant.SDK.sendVideo(sendVideo);
+    this.setState(
+      update(this.state, {
+        $merge: {
+          status: "idle"
+        }
+      })
+    );
+    this.closeModal(true);
+  }
+
+  setModalText(text, status){
+    this.setState(
+      update(this.state, {
+        $merge: {
+          modalText: text,
+          status: typeof status != "undefined" ? status : this.state.status,
+          isModalOpen: true
+        }
+      })
+    );
+  }
+
+  onPressBackdrop(){
+    if(this.state.status != "inboundcall") this.closeModal();
+  }
+
+  switchKeypad(){
+    if(this.state.status == 'connected'){
+      this.setState(
+        update(this.state, {
+          $merge: {
+            status: "connected_keypad"
+          }
+        })
+      );
+    } else {
+      this.setState(
+        update(this.state, {
+          $merge: {
+            status: "connected"
+          }
+        })
+      );
+    }
+  }
+
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
